0a5a8df00daec5c62e3231e2b2192880ae4c2eb8
AdarshCHMMMUT <97625989+AdarshCHMMMUT@users.noreply.github.com>
2025-06-09 12:11:28 +0530

    removed the complex auth setup used a simple login with compare

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String, required:true,unique:true},
-    password:{type:String, required:true},
-    verifyOtp: {type:String, default:''},
-    verifyOtpExpireAt: {type: Number, default: 0},
-    isAccountVerified: {type: Boolean, default: false},
-    resetotp: {type:String, default : ''},
-    resetOtpExpireAt: {type: Number, default: 0},
-})
-
+  phone: { type: String, required: true },
+  role: { type: String, enum: ['subadmin', 'admin', 'manager'], default: 'subadmin' },
+  password: { type: String, required: true }
+});
 const userModel =  mongoose.models.user || mongoose.model('users',userSchema);
 
 export default userModel;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,17 +1,10 @@
 import express from "express";
-import { isAuthenticated, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from "../controller/authController.js";
-import userAuth from "../middleware/userAuth.js";
+import { adminlogin } from "../controller/authController.js";
+ 
 
 const authRouter = express.Router();
 
-authRouter.post('/register',register);
-authRouter.post('/login',login);
-authRouter.post('/logout',logout);
-authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp);
-authRouter.post('/verify-account',userAuth,verifyEmail);
-authRouter.post('/is-auth',userAuth,isAuthenticated);
-authRouter.post('/send-reset-otp',sendResetOtp);
-authRouter.post('/reset-password',resetPassword);
-
+authRouter.post('/login',adminlogin);
+ 
 
 export default authRouter;
